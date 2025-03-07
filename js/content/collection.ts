6ccee68036f8805c965044123548a1783c5cefbc
Allan Jardine <allan@datatables.net>
2025-03-07 15:26:01 +0000

    Dev: Forgot one instance of a function

--- js/content/collection.ts
+++ js/content/collection.ts
@@ -164,7 +164,7 @@ export default {
 			.icon(config.icon)
 			.className(config.className)
 			.handler(() => {
-				closeOthers();
+				close();
 
 				if (dropdown._shown) {
 					dropdown._close();
