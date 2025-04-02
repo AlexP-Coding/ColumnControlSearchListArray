a2254baa89dfee8991b7fe25332f765023459e75
Allan Jardine <allan@datatables.net>
2025-04-02 15:40:10 +0000

    Column reordering integration with a button that can be used to drag columns

--- js/content/reorder.ts
+++ js/content/reorder.ts
@@ -1,6 +1,8 @@
 import Button from '../Button';
 import {IContentPlugin, IContentConfig} from './content';
 
+declare var DataTable: any;
+
 export interface IReorder extends IContentConfig {
 	className: string;
 	icon: string;
@@ -35,6 +37,11 @@ export default {
 		dt.on('columns-reordered', (e, details) => {
 			btn.enable(this.idx() > 0);
 		});
+
+		// If ColReorder wasn't initialised on this DataTable, then we need to add it
+		if (! (dt.init() as any).colReorder) {
+			new DataTable.ColReorder(dt, {});
+		}
 
 		return btn.element();
 	}
