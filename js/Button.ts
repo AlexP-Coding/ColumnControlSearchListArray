8ed0e0aae0979a2b1d553ea59077c6daa5a6144c
Allan Jardine <allan@datatables.net>
2025-06-17 15:00:50 +0000

    Fix: Set `aria-label` for buttons

--- js/Button.ts
+++ js/Button.ts
@@ -246,6 +246,8 @@ export default class Button {
 		this._dom.text.innerHTML = text;
 		this._s.label = text; // for fast retrieval
 
+		this._dom.button.setAttribute('aria-label', text);
+
 		return this;
 	}
 
